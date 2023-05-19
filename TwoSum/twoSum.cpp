2a0f5d03f857a773e47ab8f971185da4ddc55281
Danil <131797470+DanilSirazeev@users.noreply.github.com>
2023-05-19 19:57:25 +0300

    Update twoSum.cpp
    Add task description

--- TwoSum/twoSum.cpp
+++ TwoSum/twoSum.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target)
