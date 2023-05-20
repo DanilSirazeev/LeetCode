ce31188ef50cc1ffb8aed0469ba77a780f2131f9
DanilSirazeev <j_dnsirazeev@mail.ru>
2023-05-20 23:42:07 +0300

    update task TwoSum

--- TwoSum/twoSum.cpp
+++ TwoSum/twoSum.cpp
@@ -1,47 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 //Input: nums = [2,7,11,15], target = 9
 //Output: [0,1]
 
-class Solution {
-public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target)
-    {
-        std::vector<int32_t> vec;
-
-        for (int i = 0; i < nums.size() - 1; ++i)
-        {
-            for (int j = i + 1; j < nums.size(); ++j)
-            {
-                if (nums[i] + nums[j] == target)
-                {
-                    vec.push_back(i);
-                    vec.push_back(j);
-                }
-            }
-        }
-
-        return vec;
-    }
-
-    void print_vec(const std::vector<int32_t>& vec)
-    {
-        for (const auto& i : vec)
-        {
-            std::cout << i;
-        }
-    }
-};
-
-int main(int argv, char** argc)
+std::vector<int32_t> twoSum(std::vector<int>& nums, int32_t target)
 {
-    std::vector<int32_t> vec{3,2,3};
+    std::sort(nums.begin(), nums.end());
 
-    Solution obj;
+    for (int32_t i = 0, j = nums.size()- 1; i < j;)
+    {
+        if (nums[i] + nums[j] == target)
+        {
+            return {i, j};
+        }
 
-    obj.print_vec(obj.twoSum(vec, 6));
+        if (nums[i] + nums[j] > target)
+        {
+            --j;
+        }
+        else
+        { 
+            ++i;
+        }
+    }
 
+    return {};
+}
+
+int32_t main(int32_t argv, char** argc)
+{
+    std::vector<int32_t> vec{7, 2, 15, 11};
+
+    auto result = twoSum(vec, 18);
+
+    if (!result.empty())
+    {
+        std::cout << result.front() << " " << result.back() << "\n";
+    }
+    
     return 0;
 }
